d8e9333a27daf5679005ad0e53db7cc7cb3dd899
kingfisheii <132377565+kingfisheii@users.noreply.github.com>
2023-12-14 08:24:51 -0500

    a/;kfdma?

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -4,16 +4,23 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldListCell;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Popup;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.util.ArrayList;
 
@@ -22,8 +29,6 @@ import java.util.ArrayList;
 public class HelloController {
     @FXML
     private Label welcomeText;
-
-
     // feilds for controllers
     public TextField myTextFeild;
     public ListView myListView;
@@ -32,70 +37,82 @@ public class HelloController {
     public RadioButton myRadio;
     public DatePicker myDatePicker;
     public ColorPicker myColorPicker;
-    public Pane myPane;;
+    public Pane myPane;
+    public Button popupbutton;
+
+    public TableView myTableView;
     //1 initliaze method
 
     //method for all on acction.
-
-
-
     public void TextOnAction() {
 
         System.out.println(myTextFeild.getText());
         System.out.println(myDatePicker.getValue().toString());
 
-        myListView.getItems().add(myTextFeild.getText() +"   "+myDatePicker.getValue().toString());
+        myListView.getItems().add(myTextFeild.getText() + "   " + myDatePicker.getValue().toString());
 
     }
-public void ButtonRemove(){
-    myListView.getItems().remove(myListView.getSelectionModel().getSelectedIndex());
-    System.out.println(myListView.getSelectionModel().getSelectedIndex());
-    OverListView.getItems().addAll(myListView.getSelectionModel().getSelectedItem());
-}
-    public void Reset(){
+
+    public void popup() {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Popup");
+        ButtonType type = new ButtonType("Ok", ButtonBar.ButtonData.OK_DONE);
+        alert.setContentText("This is a Popup");
+        alert.getDialogPane().getButtonTypes().add(type);
+        Text txt = new Text("Click the button to show the dialog");
+        alert.showAndWait();
+
+    }
+
+    public void ButtonRemove() {
+        myListView.getItems().remove(myListView.getSelectionModel().getSelectedIndex());
+        System.out.println(myListView.getSelectionModel().getSelectedIndex());
+        OverListView.getItems().addAll(myListView.getSelectionModel().getSelectedItem());
+    }
+
+    public void Reset() {
         myListView.getItems().clear();
         OverListView.getItems().addAll(myListView.getItems());
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Cleared");
+        ButtonType type = new ButtonType("Ok", ButtonBar.ButtonData.OK_DONE);
+        alert.setContentText("You Cleared The List");
+        alert.getDialogPane().getButtonTypes().add(type);
+        Text txt = new Text("Click the button to show the dialog");
+        alert.showAndWait();
+
     }
-    public void donetask(){
+
+    public void donetask() {
         myListView.getItems().remove(myListView.getSelectionModel().getSelectedIndex());
-DoneListView.getItems().addAll(myListView.getSelectionModel().getSelectedItem());
+        DoneListView.getItems().addAll(myListView.getSelectionModel().getSelectedItem());
     }
-    public void restore(){
+
+    public void restore() {
         OverListView.getItems().remove(OverListView.getSelectionModel().getSelectedItem());
         myListView.getItems().add(OverListView.getSelectionModel().getSelectedItem());
     }
-    public void texteddit(){
 
-        if(myRadio.isSelected()){
+    public void texteddit() {
 
+        if (myRadio.isSelected()) {
+            System.out.println("AHL<FJSAHLASKD");
             myListView.setEditable(true);
             myListView.setCellFactory(TextFieldListCell.forListView());
-        }
-        else {
+        } else {
             myListView.setEditable(false);
-
         }
+    }
+
+
+    public void tableView() {
+
 
     }
-   public void popup(){
-       Label label = new Label("This is a Popup");
 
-       // create a popup
-       Popup popup = new Popup();
-
-       // set background
-
-       // add the label
-       popup.getContent().add(label);
-
-       // set size of label
-       label.setMinWidth(80);
-       label.setMinHeight(50);
-
-
-
-   }
-    }
+}
 
 
 
